8820a601a2ea8988f7f8819188c4fcbd8619264c
arruemario97 <59886358+arruemario97@users.noreply.github.com>
2020-01-16 17:09:01 +0100

    Update rgb_servo.ino

--- rgb_servo/rgb_servo.ino
+++ rgb_servo/rgb_servo.ino
@@ -6,6 +6,7 @@
 Servo servomotor1;
 Servo servomotor2;
 uint16_t total_r, total_g, total_b, total_c;
+//Limites de cada color
 uint16_t black_limit_values[] = {0, 200, 0, 200, 0, 200};
 uint16_t yellow_limit_values[] = {167, 220, 183, 251, 77, 102};
 uint16_t green_limit_values[] = {55, 70, 115, 175, 47, 67};
@@ -14,26 +15,28 @@ uint16_t red_limit_values[] = {90, 140, 50, 100, 70, 125};
 uint16_t purple_limit_values[] = {0, 30, 0, 35, 0, 35};
 int grado=0;
 float sum;
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_1X);  // setup RGB sensor
+// setup RGB sensor
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_1X);
   
 void setup() {
 Serial.begin(9600);
 while (!Serial) {};
 Serial.println("Red,Green,Blue");
-servomotor1.attach(4);
+servomotor1.attach(4);//Configuración salidas servo
 servomotor2.attach(5);
 }
 
 void loop() { 
-  servomotor1.write(0);
+  servomotor1.write(0); //Servo 1 a 0
   delay(1000);
-  servomotor1.write(90);
+  servomotor1.write(90); //Servo 1 posición lectura
   delay(500);
   // wait for proximity and color sensor data
   if (tcs.begin()) {    //check if sensor found
     Serial.println("Found sensor");
+    // Obtención de datos RGB
     tcs.getRawData(&total_r, &total_g, &total_b, &total_c);
-     Serial.print(total_r);
+    Serial.print(total_r);
     Serial.print(',');
     Serial.print(total_g);
     Serial.print(',');
@@ -41,7 +44,7 @@ void loop() {
     Serial.println();
     delay (2000);
    }
- 
+ //Clasificación de color en función del RGB
     if ((total_r >= yellow_limit_values[0]) &&   //check for yellow
            (total_r < yellow_limit_values[1]) &&
            (total_g >= yellow_limit_values[2]) &&
@@ -87,8 +90,8 @@ void loop() {
     {Serial.print("No identificado");
     grado=10;}
     Serial.println();
-    servomotor2.write(grado);
+    servomotor2.write(grado); // Giro posicionador
     delay (500);
-    servomotor1.write(180);
+    servomotor1.write(180); // Deposición Skittles
     delay(1000);
   }
